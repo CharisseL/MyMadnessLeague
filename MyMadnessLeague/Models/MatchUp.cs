b1fa6fab7def1e4a5918b089f2d2a0a041da7763
CharisseL <charisse8@gmail.com>
2015-03-13 08:02:14 -0500

    remake MatchUp model

---
+++ MyMadnessLeague/Models/MatchUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyMadnessLeague.Models
+{
+    public class MatchUp
+    {
+        public int MatchUpId { get; set; }
+        public string Team1 { get; set; }
+        public string Team2 { get; set; }
+        public string MatchWinner { get; set; }
+
+        public MatchUp()
+        {
+
+        }
+
+    }
+}

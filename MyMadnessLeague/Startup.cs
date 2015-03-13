e45024c66f6322f6496b975d60de33a6fdb26139
CharisseL <charisse8@gmail.com>
2015-03-13 05:35:07 -0500

    solution setup

---
+++ MyMadnessLeague/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyMadnessLeague.Startup))]
+namespace MyMadnessLeague
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
